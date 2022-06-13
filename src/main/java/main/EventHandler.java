d47aa72c65b49a90a228ce18315f037c257dfb78
Luki1235512 <Luki1235512@gmail.com>
2022-06-13 13:38:27 +0200

    eventHandler rework

--- src/main/java/main/EventHandler.java
+++ src/main/java/main/EventHandler.java
@@ -1,57 +1,84 @@
 package main;
 
-import java.awt.*;
-
 public class EventHandler {
 
     private final GamePanel gamePanel;
-    private final Rectangle eventRect;
-    private final int eventRectDefaultX;
-    private final int eventRectDefaultY;
+    private final EventRect[][] eventRect;
+
+    private int previousEventX;
+    private int previousEventY;
+    private boolean canTouchEvent = true;
 
     public EventHandler(GamePanel gamePanel) {
         this.gamePanel = gamePanel;
 
-        eventRect = new Rectangle();
-        eventRect.x = 23;
-        eventRect.y = 23;
-        eventRect.width = 2;
-        eventRect.height = 2;
-        eventRectDefaultX = eventRect.x;
-        eventRectDefaultY = eventRect.y;
+        eventRect = new EventRect[gamePanel.getMaxWorldCol()][gamePanel.getMaxWorldRow()];
+        int col = 0;
+        int row = 0;
+        while (col < gamePanel.getMaxWorldCol() && row < gamePanel.getMaxWorldRow()) {
+            eventRect[col][row] = new EventRect();
+            eventRect[col][row].x = 23;
+            eventRect[col][row].y = 23;
+            eventRect[col][row].width = 2;
+            eventRect[col][row].height = 2;
+            eventRect[col][row].eventRectDefaultX = eventRect[col][row].x;
+            eventRect[col][row].eventRectDefaultY = eventRect[col][row].y;
+
+            col++;
+            if (col == gamePanel.getMaxWorldCol()) {
+                col = 0;
+                row++;
+            }
+        }
+
+
     }
 
     public void checkEvent() {
 
-        if (hit(27, 16, "right")) {
-            damagePit(gamePanel.getDialogueState());
+        int distanceX = Math.abs(gamePanel.getPlayer().getWorldX() - previousEventX);
+        int distanceY = Math.abs(gamePanel.getPlayer().getWorldY() - previousEventY);
+        int distance = Math.max(distanceX, distanceY);
+        if (distance > gamePanel.getTileSize()) {
+            canTouchEvent = true;
         }
-        if (hit(23, 12, "up")) {
-            healingPool(gamePanel.getDialogueState());
-        }
+
+        if (canTouchEvent) {
+            if (hit(27, 16, "right")) {
+                damagePit(gamePanel.getDialogueState());
+            }
+            if (hit(23, 12, "up")) {
+                healingPool(gamePanel.getDialogueState());
+            }
 //        if (hit(27, 16, "right")) {
-//            teleport(gamePanel.getDialogueState());
+//            teleport(27, 16, gamePanel.getDialogueState());
 //        }
+        }
+
+
     }
 
-    public boolean hit(int eventCol, int eventRow, String reqDirection) {
+    public boolean hit(int col, int row, String reqDirection) {
 
         boolean hit = false;
 
         gamePanel.getPlayer().getSolidArea().x = gamePanel.getPlayer().getWorldX() + gamePanel.getPlayer().getSolidArea().x;
         gamePanel.getPlayer().getSolidArea().y = gamePanel.getPlayer().getWorldY() + gamePanel.getPlayer().getSolidArea().y;
-        eventRect.x = eventCol * gamePanel.getTileSize() + eventRect.x;
-        eventRect.y = eventRow * gamePanel.getTileSize() + eventRect.y;
+        eventRect[col][row].x = col * gamePanel.getTileSize() + eventRect[col][row].x;
+        eventRect[col][row].y = row * gamePanel.getTileSize() + eventRect[col][row].y;
 
-        if (gamePanel.getPlayer().getSolidArea().intersects(eventRect)) {
+        if (gamePanel.getPlayer().getSolidArea().intersects(eventRect[col][row]) && !eventRect[col][row].eventDone) {
             if (gamePanel.getPlayer().getDirection().contentEquals(reqDirection) || reqDirection.contentEquals("any")) {
                 hit = true;
+
+                previousEventX = gamePanel.getPlayer().getWorldX();
+                previousEventY = gamePanel.getPlayer().getWorldY();
             }
         }
         gamePanel.getPlayer().getSolidArea().x = gamePanel.getPlayer().getSolidAreaDefaultX();
         gamePanel.getPlayer().getSolidArea().y = gamePanel.getPlayer().getSolidAreaDefaultY();
-        eventRect.x = eventRectDefaultX;
-        eventRect.y = eventRectDefaultY;
+        eventRect[col][row].x = eventRect[col][row].eventRectDefaultX;
+        eventRect[col][row].y = eventRect[col][row].eventRectDefaultY;
 
         return hit;
     }
@@ -67,6 +94,7 @@ public class EventHandler {
         gamePanel.setGameState(gameState);
         gamePanel.getUi().setCurrentDialogue("You fall int a pit!");
         gamePanel.getPlayer().decreaseLife(1);
+        canTouchEvent = false;
     }
 
     public void healingPool(int gameState) {
