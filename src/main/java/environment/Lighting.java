4e37dd1c5df64f0f496e4e02f3997ed651e83703
Luki1235512 <Luki1235512@gmail.com>
2022-07-15 14:44:14 +0200

    Gradient effect

--- src/main/java/environment/Lighting.java
+++ src/main/java/environment/Lighting.java
@@ -33,7 +33,42 @@ public class Lighting {
 
         screenArea.subtract(lightArea);
 
-        g2.setColor(new Color(0, 0, 0, 0.95f));
+        Color[] color = new Color[12];
+        float[] fraction = new float[12];
+
+        color[0] = new Color(0, 0, 0, 0.1f);
+        color[1] = new Color(0, 0, 0, 0.42f);
+        color[2] = new Color(0, 0, 0, 0.52f);
+        color[3] = new Color(0, 0, 0, 0.61f);
+        color[4] = new Color(0, 0, 0, 0.69f);
+        color[5] = new Color(0, 0, 0, 0.76f);
+        color[6] = new Color(0, 0, 0, 0.82f);
+        color[7] = new Color(0, 0, 0, 0.87f);
+        color[8] = new Color(0, 0, 0, 0.91f);
+        color[9] = new Color(0, 0, 0, 0.94f);
+        color[10] = new Color(0, 0, 0, 0.96f);
+        color[11] = new Color(0, 0, 0, 0.98f);
+
+        fraction[0] = 0f;
+        fraction[1] = 0.4f;
+        fraction[2] = 0.5f;
+        fraction[3] = 0.6f;
+        fraction[4] = 0.65f;
+        fraction[5] = 0.7f;
+        fraction[6] = 0.75f;
+        fraction[7] = 0.8f;
+        fraction[8] = 0.85f;
+        fraction[9] = 0.9f;
+        fraction[10] = 0.95f;
+        fraction[11] = 1f;
+
+        RadialGradientPaint rgp = new RadialGradientPaint(centerX, centerY, (circleSize / 2), fraction, color);
+
+        g2.setPaint(rgp);
+
+        g2.fill(lightArea);
+
+//        g2.setColor(new Color(0, 0, 0, 0.95f));
 
         g2.fill(screenArea);
         g2.dispose();
